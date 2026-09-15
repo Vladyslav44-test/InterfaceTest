37bd1ddc510f5a240000cbf521d8df35cd2396e3
Vladislav44-test <elertvelert@gmail.com>
2026-09-15 16:48:26 +0300

    Додав клас TallGuy, який використовується в методі Main та який реалізує інтерфейс IClown

---
+++ TallGuy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTest
+{
+    internal class TallGuy
+    {
+        public string Name;
+        public int Height;
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+        }
+    }
+}
