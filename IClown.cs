37cea4cae75bf6b71f3d97ae4e1b75270dab9373
Vladislav44-test <elertvelert@gmail.com>
2026-09-15 16:54:47 +0300

    Додав інтерфейс IClown

---
+++ IClown.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTest
+{
+    internal interface IClown
+    {
+        string FunnyThingIHave { get; }
+
+        void Honk();
+    }
+}
