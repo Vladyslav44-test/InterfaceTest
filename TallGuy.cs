3ba44aa8a8adff1a667d89fe43b35ee25f725fb6
Vladislav44-test <elertvelert@gmail.com>
2026-09-15 16:58:52 +0300

    Змінив клас TallGuy, щоб він коректно реалізував інтерфейс IClown

--- TallGuy.cs
+++ TallGuy.cs
@@ -6,14 +6,20 @@ using System.Threading.Tasks;
 
 namespace InterfaceTest
 {
-    internal class TallGuy
+    internal class TallGuy : IClown
     {
         public string Name;
         public int Height;
 
+        public string FunnyThingIHave { get { return "big shoes"; } }
+
         public void TalkAboutYourself()
         {
             Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+        }
+        public void Honk()
+        {
+            Console.WriteLine("Honk honk!");
         }
     }
 }
